6b8fd09e6f6c68211d2cdfeb1ce366e8d04c64e3
ncaldwell17 <35404060+ncaldwell17@users.noreply.github.com>
2018-10-10 13:27:25 -0500

    Add files via upload

---
+++ ID3.py
@@ -0,0 +1,43 @@
+from node import Node
+import math
+
+def ID3(examples, default):
+  if examples is empty
+    return tree(default)
+  else if all examples have same classification or no non-trivial splits are possible
+    return tree(MODE(examples))
+  else:
+    best <- CHOOSE-ATTRIBUTE(examples)
+    t <- new tree with root test best
+    for each value of best:
+      examples <- {elements of examples with best}
+      subtree <- IDS(examplesi, MODE(examples))
+      add branch to t with label value and subtree subtree
+    return t
+  '''
+  Takes in an array of examples, and returns a tree (an instance of Node)
+  trained on the examples.  Each example is a dictionary of attribute:value pairs,
+  and the target class variable is a special attribute with the name "Class".
+  Any missing attributes are denoted with a value of "?"
+  '''
+
+
+
+def prune(node, examples):
+  '''
+  Takes in a trained tree and a validation set of examples.  Prunes nodes in order
+  to improve accuracy on the validation data; the precise pruning strategy is up to you.
+  '''
+
+def test(node, examples):
+  '''
+  Takes in a trained tree and a test set of examples.  Returns the accuracy (fraction
+  of examples the tree classifies correctly).
+  '''
+
+
+def evaluate(node, example):
+  '''
+  Takes in a tree and one example.  Returns the Class value that the tree
+  assigns to the example.
+  '''
---
+++ node.py
@@ -0,0 +1,43 @@
+class Node:
+  def __init__(self):
+    self.label = None
+    self.children = {}
+
+# just add some additional fields
+
+"""attributes = {"vote":y
+              "vote":n
+              "vote":?}
+
+attributes = {
+  "n" : y,
+  "n2" : n,
+  "n3" : ?,
+}
+
+class Tree:
+    self.dictionary = {
+      ["attributes" : y, n, ?]
+    (tree, dict(a=1, b=0))
+    }
+
+class vote(object):
+  def __init__(self, politician, vote):
+    self.politician = politician
+    self.vote = vote
+            dict(vote=0, vote=1, Class=Party)
+
+politician = [Politician("1", "Republican")]
+
+class response(object):
+  def __init__(self, answer):
+    self.answer = y, n, or ?
+
+# Empty dict
+r = {}
+# fill in entries one by one
+r["response"] = y
+r["response"] = n
+r["response"] = ?"""
+
+	# you want to add additional fields here...
